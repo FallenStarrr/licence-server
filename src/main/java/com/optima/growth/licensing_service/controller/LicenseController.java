480ab4a65f3290762e79692389fd0ac5fff9384b
FallenStarrr <juzopigeon@gmail.com>
2024-09-07 22:56:59 +0600

    License model

---
+++ src/main/java/com/optima/growth/licensing_service/controller/LicenseController.java
@@ -0,0 +1,13 @@
+package com.optima.growth.licensing_service.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping(value="v1/organization/{organizationId}/license")
+public class LicenseController {
+  
+}
